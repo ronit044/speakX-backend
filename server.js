66e57fc03ec2f582dde6a105fc40a7f76331923e
Ronit Sharma <ronitsharma3870@gmail.com>
2025-01-21 15:00:34 +0530

    major workflow changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const grpcServer=require('./routes/grpcRoutes');
 const {connectDB}=require('./app'); 
 require('dotenv').config();
 // connectDB();
-const PORT = process.env.PORT || 50051;
+const PORT = process.env.PORT || 8585;
 grpcServer.bindAsync(`0.0.0.0:${PORT}`,grpc.ServerCredentials.createInsecure(),(error) => {
   if (error) {
     console.error('error occuring in bind async part:->>', error);
